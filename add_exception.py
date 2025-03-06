459b5e63bdbc0208c428737365c1b606125d6b7d
d.gurov <gurov98den@gmal.com>
2025-03-06 16:17:56 +0300

    'C:\\Program Files\\PHSV Helper'

--- add_exception.py
+++ add_exception.py
@@ -2,6 +2,6 @@ import subprocess
 
 def add_exception():
     program_folder ='''
-    Add-MpPreference -ExclusionPath 'C:\\Program Files\\AdmHelper\\AdmHelper'
+    Add-MpPreference -ExclusionPath 'C:\\Program Files\\PHSV Helper'
     '''
     subprocess.run(['powershell', f'{program_folder}'])
