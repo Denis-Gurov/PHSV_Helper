65c0e975958b0b3ec1e53415a2bc8799d648721b
d.gurov <gurov98den@gmal.com>
2025-03-06 15:59:50 +0300

    Updater 1.1

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\users\d.gurov\appdata\local\programs\python\python312;%PATH%
+set PYTHONHOME=C:\Users\d.gurov\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\d.gurov\PycharmProjects\PHSV Helper;C:\Users\d.gurov\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\d.gurov\AppData\Local\Programs\Python\Python312\Lib;C:\Users\d.gurov\AppData\Local\Programs\Python\Python312;C:\Users\d.gurov\PycharmProjects\PassMass\.venv;C:\Users\d.gurov\PycharmProjects\PassMass\.venv\Lib\site-packages;C:\Users\d.gurov\PycharmProjects\PassMass\.venv\Lib\site-packages\win32;C:\Users\d.gurov\PycharmProjects\PassMass\.venv\Lib\site-packages\win32\lib;C:\Users\d.gurov\PycharmProjects\PassMass\.venv\Lib\site-packages\Pythonwin;C:\Users\d.gurov\PycharmProjects\PassMass\.venv\Lib\site-packages\setuptools\_vendor
+"%~dp0main.exe" %*
